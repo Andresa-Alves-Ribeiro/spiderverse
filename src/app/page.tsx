cc075d0287e4f4de8d50411fb46d53aae5a4828a
Andresa Alves <andresa_15ga@hotmail.com>
2023-07-01 17:12:08 -0300

    adicionando useEffect e useState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,39 @@
-import styles from "./page.module.scss";
+import { useEffect, useState } from 'react';
 
-import HeroesList from "@/components/HeroesList";
-import { IHeroData } from "@/interfaces/heroes";
+import styles from './page.module.scss';
+
+import HeroesList from '@/components/HeroesList';
+import { IHeroData } from '@/interfaces/heroes';
 
 async function getData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch("/api/heroes");
+  const res = await fetch('/api/heroes');
 
   if (!res.ok) {
-    throw new Error("Falha ao buscar heróis");
+    throw new Error('Falha ao buscar heróis');
   }
 
   return res.json();
 }
 
-export default async function Home() {
-  const res = await getData();
+export default function Home() {
+  const [heroes, setHeroes] = useState<IHeroData[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getData();
+        setHeroes(data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <main className={styles.main}>
-      <HeroesList heroes={res.data} />
+      <HeroesList heroes={heroes} />
     </main>
   );
 }
