12c6977c1ac174fb7d70b473dd0c61c80f19d5fb
Andresa Alves <andresa_15ga@hotmail.com>
2023-07-01 17:34:19 -0300

    Modificando fetch do hero

--- src/app/hero/[id]/page.tsx
+++ src/app/hero/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Carousel from "@/components/Carousel";
 import { IHeroData } from "@/interfaces/heroes";
 
@@ -8,7 +10,7 @@ interface IProps {
 }
 
 async function getData(): Promise<{ data: IHeroData[] }> {
-  const res = await fetch("http://localhost:3000/api/heroes");
+  const res = await fetch('/api/heroes');
 
   if (!res.ok) {
     throw new Error("Falha ao buscar heróis");
